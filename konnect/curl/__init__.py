de837b478c50eb6c3be27eb045e0b7e244de82e7
Dom Sekotill <dom.sekotill@kodo.org.uk>
2023-09-04 01:47:58 +0100

    Fix explicit reexport in top-level

--- konnect/curl/__init__.py
+++ konnect/curl/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2023  Dom Sekotill <dom.sekotill@kodo.org.uk>
 
-from ._enums import MILLISECONDS
-from ._enums import SECONDS
+from ._enums import MILLISECONDS as MILLISECONDS
+from ._enums import SECONDS as SECONDS
 from ._enums import Time as Time
 from ._exceptions import CurlError as CurlError
 from ._multi import Multi as Multi
