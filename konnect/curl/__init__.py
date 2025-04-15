7fee3be2154e16e24fa994e371c3ed34ff7be4ec
Dom Sekotill <dom.sekotill@kodo.org.uk>
2025-04-15 18:25:17 +0100

    Use __all__ to control unimport behaviour

--- konnect/curl/__init__.py
+++ konnect/curl/__init__.py
@@ -1,8 +1,17 @@
-# Copyright 2023  Dom Sekotill <dom.sekotill@kodo.org.uk>
+# Copyright 2023, 2025  Dom Sekotill <dom.sekotill@kodo.org.uk>
 
-from ._enums import MILLISECONDS as MILLISECONDS
-from ._enums import SECONDS as SECONDS
-from ._enums import Time as Time
-from ._exceptions import CurlError as CurlError
-from ._multi import Multi as Multi
-from .requests import Request as Request
+from ._enums import MILLISECONDS
+from ._enums import SECONDS
+from ._enums import Time
+from ._exceptions import CurlError
+from ._multi import Multi
+from .requests import Request
+
+__all__ = [
+	"MILLISECONDS",
+	"SECONDS",
+	"CurlError",
+	"Multi",
+	"Request",
+	"Time",
+]
